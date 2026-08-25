967549f3f03cd56d7ef347bd4d3c74e8083f5048
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2026-08-25 17:30:24 +0000

    bignum for diffie-hellman

--- client/dh.h
+++ client/dh.h
@@ -1,5 +1,7 @@
 #include <openssl/bn.h>
 
+
+
 void sq_mult(const BIGNUM *g, const BIGNUM *a, const BIGNUM *p, BIGNUM *share, BN_CTX *ctx);
 
 void secret_maker(const BIGNUM *ga, const BIGNUM *b, const BIGNUM *p, BIGNUM *secret, BN_CTX *ctx);
